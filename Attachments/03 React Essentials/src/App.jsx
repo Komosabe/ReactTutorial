be94120de9f42770ffe4486709f717405e4a7d80
Maciek Pietrasz <73303606+Komosabe@users.noreply.github.com>
2024-02-22 19:07:21 +0100

    03.53
    Managing State & Using Hooks [Core Concept]

--- Attachments/03 React Essentials/src/App.jsx
+++ Attachments/03 React Essentials/src/App.jsx
@@ -1,12 +1,17 @@
-import componentsImg from "./assets/components.png";
+import { useState } from "react";
+
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState("Please click a button");
+
+  let tabContent = "Please click a button";
+
   function handleSelect(selectedButton) {
-    (selectedButton) => "components", "jsx", "props", "state";
+    setSelectedTopic(selectedButton);
   }
 
   return (
@@ -36,6 +41,7 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
+          {selectedTopic}
         </section>
       </main>
     </div>
