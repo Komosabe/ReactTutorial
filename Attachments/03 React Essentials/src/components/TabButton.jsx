b780543c4107ce97d97bb7f61417c126c535b8c3
Maciek Pietrasz <73303606+Komosabe@users.noreply.github.com>
2024-02-22 21:02:50 +0100

    03.56
    CSS Styling & Dynamic Styling

--- Attachments/03 React Essentials/src/components/TabButton.jsx
+++ Attachments/03 React Essentials/src/components/TabButton.jsx
@@ -1,10 +1,9 @@
-export default function TabButton({ children, onSelect }) {
-  function handleClick() {
-    console.log("hello");
-  }
+export default function TabButton({ children, onSelect, isSelected }) {
   return (
     <li>
-      <button onClick={onSelect}> {children} </button>
+      <button className={isSelected ? "active" : undefined} onClick={onSelect}>
+        {children}
+      </button>
     </li>
   );
 }
