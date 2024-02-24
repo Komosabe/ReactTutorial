74bc2c18231d6ee778580d173ee75129c51f3c1e
Maciek Pietrasz <73303606+Komosabe@users.noreply.github.com>
2024-02-24 23:50:37 +0100

    04.74
    Concept Repetition: Working with State

--- Attachments/04 React Essentials - Deep Dive/src/components/Player.jsx
+++ Attachments/04 React Essentials - Deep Dive/src/components/Player.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, seIsEditing] = useState(false);
+
+  function handleEditClick() {
+    seIsEditing(true);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+
+  if (isEditing === true) {
+    playerName = <input type="text" required />;
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
+
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>Edit</button>
     </li>
   );
 }
