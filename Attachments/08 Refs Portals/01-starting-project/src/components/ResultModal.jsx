44aee2c07d781523aa8106e705368f6132ede325
Maciek Pietrasz <73303606+Komosabe@users.noreply.github.com>
2024-03-21 18:07:05 +0100

    08.142
    Sharing State Across Components

--- Attachments/08 Refs Portals/01-starting-project/src/components/ResultModal.jsx
+++ Attachments/08 Refs Portals/01-starting-project/src/components/ResultModal.jsx
@@ -1,10 +1,13 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const ResultModal = forwardRef(function ResultModal(
-  { result, targetTime },
+  { targetTime, remainingTime, onReset },
   ref
 ) {
   const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
   useImperativeHandle(ref, () => {
     return {
@@ -16,14 +19,15 @@ const ResultModal = forwardRef(function ResultModal(
 
   return (
     <dialog ref={dialog} class="result-modal">
-      <h2>You {result}</h2>
+      {userLost && <h2>You lost </h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <string>X seconds left.</string>
+        You stopped the timer with{" "}
+        <string>{formattedRemainingTime} seconds left.</string>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
